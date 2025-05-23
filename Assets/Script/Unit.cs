2be36b4356f42985bf3a87432571dcfcdb8cc67d
Jhonatan Reis Tavares <49035693+jhonatanreistavares@users.noreply.github.com>
2025-05-23 16:58:26 -0400

    Last Commit off the day 23/05

--- Assets/Script/Unit.cs
+++ Assets/Script/Unit.cs
@@ -14,10 +14,11 @@ public class Unit : MonoBehaviour
         targetPosition = transform.position;
     }
     
-    private void Start()
+   /* private void Start()
     {
         LevelGrid.Instance.SetUnitAtGridPosition();
     }
+    */
     private void Update()
     {
        
